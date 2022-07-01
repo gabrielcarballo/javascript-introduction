f8a7ae9e455377706b7d80079ef7f2d1d998aa0b
gabrielcarballo <gabrielfpcarballo@gmail.com>
2022-07-01 02:20:29 -0300

    Eigth req done

--- src/challenges.js
+++ src/challenges.js
@@ -1,65 +1,78 @@
 // Desafio 1
 function compareTrue(boolean1, boolean2) {
-  if (boolean1 && boolean2){
+  if (boolean1 && boolean2) {
     return true
   } else {
     return false
-  } 
+  }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(texto) {
-   return texto.split(" ");
+  return texto.split(" ");
 }
 
 // Desafio 4
 function concatName(array) {
-  return `${array[array.length-1]}, ${array[0]}`;
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return wins*3+ties
-  
+  return wins * 3 + ties
+
 }
 
 // Desafio 6
 function highestCount(array) {
   let highest = array[0];
   let resposta = 0;
-  for (let i = 0; i < array.length; i++){
-    if (array[i] > highest){
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > highest) {
       highest = array[i];
     }
   }
-  for (let k = 0; k < array.length; k++){
-    if (array[k] === highest){
+  for (let k = 0; k < array.length; k++) {
+    if (array[k] === highest) {
       resposta += 1;
     }
-    
+
   }
- return resposta;
+  return resposta;
 }
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (cat1+1 == cat2-1 || cat1-1 == cat2+1){
-  return "os gatos trombam e o rato foge";
-  } else if (cat1-mouse < cat2-mouse){
+  if (cat1 + 1 == cat2 - 1 || cat1 - 1 == cat2 + 1) {
+    return "os gatos trombam e o rato foge";
+  } else if (cat1 - mouse < cat2 - mouse) {
     return "cat1"
-  } else if (cat2-mouse < cat1-mouse){
+  } else if (cat2 - mouse < cat1 - mouse) {
     return "cat2"
-}
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayAnswer) {
+  let resposta = [];
+  for (let i = 0; i < arrayAnswer.length; i++){
+    if (arrayAnswer[i] % 3 === 0 && arrayAnswer[i] % 5 !== 0){
+      resposta.push('fizz');
+    }else if (arrayAnswer[i] % 3 !== 0 && arrayAnswer[i] % 5 === 0){
+      resposta.push('buzz');
+    }else if (arrayAnswer[i] % 3 === 0 && arrayAnswer[i] % 5 === 0){
+      resposta.push('fizzBuzz');
+    }else if (arrayAnswer[i] % 3 !== 0 && arrayAnswer[i] % 5 !== 0){
+      resposta.push('bug!');
+    }
+  }
+  
+  return resposta;
 }
 
 // Desafio 9
